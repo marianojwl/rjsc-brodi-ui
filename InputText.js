a860c7ac52dcc1a17064fcecaedab6ef400cfdaa
Mariano Soto <marianojwl@gmail.com>
2025-12-01 00:11:10 -0300

    .

---
+++ InputText.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InputText() {
+  return (
+    <div className='InputText'>
+      <input type='text' className='form-control' />
+    </div>
+  )
+}
+
+export default InputText
