bc0f39fb2b61cf5775ef782630d93c4fcedb797b
Mariano Soto <marianojwl@gmail.com>
2025-12-04 16:31:33 -0300

    .

--- Button.js
+++ Button.js
@@ -3,10 +3,11 @@ import React from 'react'
 function Button(props) {
   const backgroundColor = "#" + (process.env?.REACT_APP_APP_BG_COLOR ?? "0d6efd");
   const color = "#" + (process.env?.REACT_APP_APP_TEXT_COLOR ?? "212529");
+  const fontWeight = 600;
   return (
     props?.href
-    ? <a className='Button btn' {...props} style={{color, backgroundColor, ...props?.style}}>{props?.label}</a>
-    : <button className='Button btn' {...props} style={{color, backgroundColor, ...props?.style}}>{props?.label}</button>
+    ? <a className='Button btn' {...props} style={{fontWeight, color, backgroundColor, ...props?.style}}>{props?.label??props?.children}</a>
+    : <button className='Button btn' {...props} style={{fontWeight, color, backgroundColor, ...props?.style}}>{props?.label??props?.children}</button>
   )
 }
 
---
+++ IconMicrosoft.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function IconMicrosoft({width="18", height="18", className=""}) {
+  return (
+    <svg className={className} xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 256 256"><path fill="#f1511b" d="M121.666 121.666H0V0h121.666z"/><path fill="#80cc28" d="M256 121.666H134.335V0H256z"/><path fill="#00adef" d="M121.663 256.002H0V134.336h121.663z"/><path fill="#fbbc09" d="M256 256.002H134.335V134.336H256z"/></svg>
+  )
+}
+
+export default IconMicrosoft
